f56a3adeceeff6b21e64a5e4512ac49416714760
Komarov Viktor Aleksandrovich <vikomarov@ozon.ru>
2022-02-20 11:40:31 +0300

    finish

--- week11/task4/update_field.cpp
+++ week11/task4/update_field.cpp
@@ -4,46 +4,6 @@
 #include <sstream>
 using namespace std;
 
-#define UPDATE_FIELD(ticket, field, values) \
-    {map<string, string>::const_iterator it = values.find(#field); \
-    if (it != values.end() ) { \
-        istringstream is(it->second); \
-        is >> ticket.field;\
-    }}
-
-void TestUpdate() {
-  AirlineTicket t;
-  t.price = 0;
-
-  const map<string, string> updates1 = {
-    {"departure_date", "2018-2-28"},
-    {"departure_time", "17:40"},
-  };
-  UPDATE_FIELD(t, departure_date, updates1);
-  UPDATE_FIELD(t, departure_time, updates1);
-  UPDATE_FIELD(t, price, updates1);
-
-  ASSERT_EQUAL(t.departure_date, (Date{2018, 2, 28}));
-  ASSERT_EQUAL(t.departure_time, (Time{17, 40}));
-  ASSERT_EQUAL(t.price, 0);
-
-  const map<string, string> updates2 = {
-    {"price", "12550"},
-    {"arrival_time", "20:33"},
-  };
-  UPDATE_FIELD(t, departure_date, updates2);
-  UPDATE_FIELD(t, departure_time, updates2);
-  UPDATE_FIELD(t, arrival_time, updates2);
-  UPDATE_FIELD(t, price, updates2);
-
-  // updates2 не содержит ключей "departure_date" и "departure_time", поэтому
-  // значения этих полей не должны измениться
-  ASSERT_EQUAL(t.departure_date, (Date{2018, 2, 28}));
-  ASSERT_EQUAL(t.departure_time, (Time{17, 40}));
-  ASSERT_EQUAL(t.price, 12550);
-  ASSERT_EQUAL(t.arrival_time, (Time{20, 33}));
-}
-
 ostream& operator<<(ostream& os, const AirlineTicket& item)
 {
   os << item.airline << std::endl;
@@ -106,8 +66,14 @@ bool operator==(const Time& lhs, const Time& rhs)
   return std::make_tuple(lhs.hours, lhs.minutes) == std::make_tuple(rhs.hours, rhs.minutes);
 }
 
+#define UPDATE_FIELD(ticket, field, values) \
+    {map<string, string>::const_iterator it = values.find(#field); \
+    if (it != values.end() ) { \
+        istringstream is(it->second); \
+        is >> ticket.field;\
+    }}
 
-int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestUpdate);
-}
+// int main() {
+//   TestRunner tr;
+//   RUN_TEST(tr, TestUpdate);
+// }
